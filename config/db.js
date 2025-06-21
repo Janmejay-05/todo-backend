c809962407bff3fc6509746fdda37b340397d518
Janmejaysinh Zala <janmejayazala@gmail.com>
2025-06-21 17:33:02 +0530

    final

--- config/db.js
+++ config/db.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 
 const connection = async () => {
-  await mongoose.connect(
-    "mongodb+srv://janmejayazala:Janmejay@2005@todo-final.yngjmzq.mongodb.net/node-final?retryWrites=true&w=majority&appName=todo-final"
-  );
-  console.log("database connected");
+  try {
+    await mongoose.connect(
+      "mongodb+srv://janmejayazala:janmejayzala@cluster0.kxf1bie.mongodb.net/node-final?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    console.log("database connected");
+  } catch (err) {
+    console.log("error =>", err);
+  }
 };
 module.exports = connection;
