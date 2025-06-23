139508a450832ad8b2ad30a7049757b3f246404f
Janmejaysinh Zala <janmejayazala@gmail.com>
2025-06-23 18:57:48 +0530

    env

--- config/db.js
+++ config/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connection = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://janmejayazala:janmejayzala@cluster0.kxf1bie.mongodb.net/node-final?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("database connected");
   } catch (err) {
     console.log("error =>", err);
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const connection = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
